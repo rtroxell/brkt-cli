02f636b7e5710f7b5a262f2281afe8f13d56fd20
Boris Burtin <boris@brkt.com>
2015-09-03 14:47:05 -0700

    NUC-9377: Add unit test infrastructure to brkt-cli
    Add infrastructure to support unit testing the end-to-end encryption
    process:
    
    * Break out the code that checks encryption progress into its own
    service.
    * Add abstract base classes for the AWS service and encryptor service.
    * Add dummy implementations of the services for unit testing.
    * Add a smoke test that runs the entire encryption process against the
    dummy services.
    * Move the code that waits for resources into brkt_cli, to make the
    service implementations leaner.
    * Don't wait for instance termination.  I think this should be safe to
    do.
    
    Change-Id: I81637f26a3318cd4d2cd1a62947f0f3d3d677b2b

--- brkt_cli/service.py
+++ brkt_cli/service.py
@@ -1,6 +1,6 @@
+import abc
 import logging
 import subprocess
-import time
 import boto
 from boto.exception import EC2ResponseError
 
@@ -22,9 +22,84 @@ def _ssh(user, external_ip, sshcommand):
     return subprocess.check_output(command, shell=True)
 
 
-class Service(object):
+class BaseAWSService(object):
+    __metaclass__ = abc.ABCMeta
 
-    def __init__(self, encryptor_session_id, encryptor_ami, default_tags=None):
+    @abc.abstractmethod
+    def run_instance(self,
+                     image_id,
+                     instance_type='m3.medium',
+                     block_device_map=None):
+        pass
+
+    @abc.abstractmethod
+    def get_instance(self, instance_id):
+        pass
+
+    @abc.abstractmethod
+    def create_tags(self, resource_id, name=None, description=None):
+        pass
+
+    @abc.abstractmethod
+    def stop_instance(self, instance_id):
+        pass
+
+    @abc.abstractmethod
+    def terminate_instance(self, instance_id):
+        pass
+
+    @abc.abstractmethod
+    def get_volume(self, volume_id):
+        pass
+
+    @abc.abstractmethod
+    def get_snapshots(self, *snapshot_ids):
+        pass
+
+    @abc.abstractmethod
+    def get_snapshot(self, snapshot_id):
+        pass
+
+    @abc.abstractmethod
+    def create_snapshot(self, volume_id, name=None, description=None):
+        pass
+
+    @abc.abstractmethod
+    def delete_volume(self, volume_id):
+        pass
+
+    @abc.abstractmethod
+    def validate_guest_ami(self, ami_id):
+        pass
+
+    @abc.abstractmethod
+    def validate_encryptor_ami(self, ami_id):
+        pass
+
+    @abc.abstractmethod
+    def register_image(self,
+                       kernel_id,
+                       block_device_map,
+                       name=None,
+                       description=None):
+        pass
+
+    @abc.abstractmethod
+    def get_image(self, image_id):
+        pass
+
+    @abc.abstractmethod
+    def delete_snapshot(self, snapshot_id):
+        pass
+
+
+class AWSService(BaseAWSService):
+
+    def __init__(
+            self,
+            encryptor_session_id,
+            encryptor_ami,
+            default_tags=None):
         self.session_id = encryptor_session_id
         self.encryptor_ami = encryptor_ami
         self.default_tags = default_tags or {}
@@ -38,63 +113,6 @@ class Service(object):
         self.key_name = key_name
         self.region = region
         self.conn = boto.vpc.connect_to_region(region)
-
-    @staticmethod
-    def get_seed_yaml(hostname):
-        for _ in range(20):
-            try:
-                return _ssh('avatar', hostname, 'cat /etc/brkt/seed.yaml')
-            except subprocess.CalledProcessError as e:
-                time.sleep(1)
-        else:
-            raise e
-
-    @staticmethod
-    def wait_for_encryptor_up(hostname):
-        st = time.time()
-        time.sleep(60)
-        for i in range(60):
-            try:
-                _ssh('avatar', hostname, 'ls')
-                break
-            except subprocess.CalledProcessError:
-                time.sleep(10)
-        else:
-            raise Exception('Unable to contact %s' % hostname)
-        et = time.time()
-        log.debug('Host is ssh-able now after %.1f seconds', (et - st))
-
-    def wait_for_instance(
-            self, instance_id, timeout=300, state='running'):
-        """ Wait for up to timeout seconds for an instance to be in the
-            'running' state.  Sleep for 2 seconds between checks.
-        :return: The Instance object, or None if a timeout occurred
-        """
-
-        start_timestamp = time.time()
-
-        log.debug(
-            'Waiting for %s, timeout=%d, state=%s',
-            instance_id, timeout, state)
-
-        # Give the AWS some time to propagate the instance creation.
-        # If we create and get immediately, AWS may return 400.  We'll fix
-        # this properly for NUC-9311.
-        time.sleep(10)
-
-        while True:
-            if (time.time() - start_timestamp) > timeout:
-                return None
-            instance = self.conn.get_only_instances(
-                instance_ids=[instance_id])[0]
-            log.debug('Instance %s state=%s', instance.id, instance.state)
-            if instance.state == state:
-                return instance
-            if instance.state == 'error':
-                raise Exception(
-                    'Instance %s is in an error state.  Cannot proceed.'
-                )
-            time.sleep(2)
 
     def run_instance(self,
                      image_id,
@@ -114,6 +132,9 @@ class Service(object):
             log.error('Unable to launch instance for %s', image_id)
             raise
 
+    def get_instance(self, instance_id):
+        return self.conn.get_only_instances([instance_id])[0]
+
     def create_tags(self, resource_id, name=None, description=None):
         tags = dict(self.default_tags)
         if name:
@@ -123,27 +144,14 @@ class Service(object):
         log.debug('Tagging %s with %s', resource_id, tags)
         self.conn.create_tags([resource_id], tags)
 
-    def stop_instance(self, instance_id, wait=True):
+    def stop_instance(self, instance_id):
         log.debug('Stopping instance %s', instance_id)
         instances = self.conn.stop_instances([instance_id])
-        if wait:
-            return self.wait_for_instance(instance_id, state='stopped')
-        else:
-            return instances[0]
+        return instances[0]
 
-    def terminate_instance(self, instance_id, wait=True):
+    def terminate_instance(self, instance_id):
         log.debug('Terminating instance %s', instance_id)
         instances = self.conn.terminate_instances([instance_id])
-        if wait:
-            return self.wait_for_instance(instance_id, state='terminated')
-        else:
-            return instances[0]
-
-    def get_instance_attribute(self, instance_id, attribute):
-        return self.conn.get_instance_attribute(
-            instance_id=instance_id,
-            attribute=attribute
-        )[attribute]
 
     def get_volume(self, volume_id):
         return self.conn.get_all_volumes(volume_ids=[volume_id])[0]
@@ -158,12 +166,6 @@ class Service(object):
         snapshot = self.conn.create_snapshot(volume_id, description)
         self.create_tags(snapshot.id, name=name)
         return snapshot
-
-    def copy_snapshot(self, snapshot_id, name=None, description=None):
-        new_snapshot_id = self.conn.copy_snapshot(
-            self.region, snapshot_id, description=description)
-        self.create_tags(new_snapshot_id, name=name)
-        return self.get_snapshot(new_snapshot_id)
 
     def delete_volume(self, volume_id):
         return self.conn.delete_volume(volume_id)
@@ -219,34 +221,56 @@ class Service(object):
             virtualization_type='paravirtual'
         )
 
-    def wait_for_image(self, image_id):
-        log.debug('Waiting for %s to become available.', image_id)
-        for i in range(180):
-            time.sleep(5)
-            try:
-                image = self.conn.get_all_images([image_id])[0]
-            except EC2ResponseError, e:
-                if e.error_code == 'InvalidAMIID.NotFound':
-                    log.debug('AWS threw a NotFound, ignoring')
-                    continue
-                else:
-                    log.warn('Unknown AWS error: %s', str(e))
-            # These two attributes are optional in the response and only
-            # show up sometimes. So we have to getattr them.
-            reason = repr(getattr(image, 'stateReason', None))
-            code = repr(getattr(image, 'code', None))
-            log.debug("%s: %s reason: %s code: %s",
-                      image.id, image.state, reason, code)
-            if image.state == 'available':
-                break
-            if image.state == 'failed':
-                raise Exception('Image state became failed')
-        else:
-            raise Exception(
-                'Image failed to become available (%s)' % (image.state,))
-
     def get_image(self, image_id):
         return self.conn.get_image(image_id)
 
     def delete_snapshot(self, snapshot_id):
         return self.conn.delete_snapshot(snapshot_id)
+
+
+class BaseEncryptorService(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        self.hostname = None
+
+    def set_hostname(self, hostname):
+        self.hostname = hostname
+
+    @abc.abstractmethod
+    def is_encryptor_up(self):
+        pass
+
+    @abc.abstractmethod
+    def get_progress(self):
+        pass
+
+
+class EncryptorService(BaseEncryptorService):
+
+    def is_encryptor_up(self):
+        if not self.hostname:
+            raise Exception('hostname has not been set')
+
+        try:
+            _ssh('avatar', self.hostname, 'ls')
+            return True
+        except subprocess.CalledProcessError, e:
+            log.debug('Could not ssh into encryptor: %s', e)
+            return False
+
+    def get_progress(self):
+        if not self.hostname:
+            raise Exception('hostname has not been set')
+
+        try:
+            seed_yaml = _ssh(
+                'avatar', self.hostname, 'cat /etc/brkt/seed.yaml'
+            )
+            if 'avatar_solo_created: true' in seed_yaml:
+                return 100
+            else:
+                return 0
+        except subprocess.CalledProcessError, e:
+            log.warn('Could not check seed.yaml: %s', e)
+            return 0
--- test.py
+++ test.py
@@ -1,10 +1,176 @@
+import logging
+import uuid
+from boto.ec2.blockdevicemapping import BlockDeviceType, BlockDeviceMapping
+from boto.ec2.image import Image
+from boto.ec2.instance import Instance
 from boto.ec2.snapshot import Snapshot
+from boto.ec2.volume import Volume
 import brkt_cli
+from brkt_cli import service
 import re
 import unittest
 
 
-class TestSoloCli(unittest.TestCase):
+brkt_cli.log = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.DEBUG)
+
+
+def _new_id():
+    return uuid.uuid4().hex[:6]
+
+
+class DummyEncryptorService(service.BaseEncryptorService):
+
+    def __init__(self):
+        self.is_up = False
+        self.progress = 0
+
+    def is_encryptor_up(self):
+        """ The first call returns False.  Subsequent calls return True.
+        """
+        ret_val = self.is_up
+        if not self.is_up:
+            self.is_up = True
+        return ret_val
+
+    def get_progress(self):
+        """ Return progress in increments of 20% for each call.
+        """
+        ret_val = self.progress
+        if self.progress < 100:
+            self.progress += 20
+        return ret_val
+
+
+class DummyAWSService(service.BaseAWSService):
+
+    def __init__(self):
+        self.instances = {}
+        self.volumes = {}
+        self.snapshots = {}
+        self.transition_to_running = {}
+        self.transition_to_completed = {}
+        self.images = {}
+
+    def run_instance(self,
+                     image_id,
+                     instance_type='m3.medium',
+                     block_device_map=None):
+        instance = Instance()
+        instance.id = _new_id()
+        instance.root_device_name = '/dev/sda1'
+        instance._state.code = 0
+        instance._state.name = 'pending'
+
+        # Create volumes based on block device data from the image.
+        image = self.get_image(image_id)
+        instance_bdm = BlockDeviceMapping()
+        for device_name, bdm in image.block_device_mapping.iteritems():
+            # Create a new volume and attach it to the instance.
+            volume = Volume()
+            volume.size = 8
+            volume.id = _new_id()
+            self.volumes[volume.id] = volume
+
+            bdt = BlockDeviceType(volume_id=volume.id, size=8)
+            instance_bdm[device_name] = bdt
+
+        instance.block_device_mapping = instance_bdm
+        self.instances[instance.id] = instance
+
+        return instance
+
+    def get_instance(self, instance_id):
+        instance = self.instances[instance_id]
+
+        # Transition from pending to running on subsequent calls.
+        if instance.state == 'pending':
+            if self.transition_to_running.get(instance_id):
+                # We returned pending last time.  Transition to running.
+                instance._state.code = 16
+                instance._state.name = 'running'
+                del(self.transition_to_running[instance_id])
+            else:
+                # Transition to running next time.
+                self.transition_to_running[instance_id] = True
+        return instance
+
+    def create_tags(self, resource_id, name=None, description=None):
+        pass
+
+    def stop_instance(self, instance_id):
+        instance = self.instances[instance_id]
+        instance._state.code = 80
+        instance._state.name = 'stopped'
+        return instance
+
+    def terminate_instance(self, instance_id):
+        instance = self.instances[instance_id]
+        instance._state.code = 48
+        instance._state.name = 'terminated'
+        return instance
+
+    def get_volume(self, volume_id):
+        return self.volumes[volume_id]
+
+    def get_snapshots(self, *snapshot_ids):
+        return [self.get_snapshot(id) for id in snapshot_ids]
+
+    def get_snapshot(self, snapshot_id):
+        snapshot = self.snapshots[snapshot_id]
+
+        # Transition from pending to completed on subsequent calls.
+        if snapshot.status == 'pending':
+            if self.transition_to_completed.get(snapshot_id):
+                # We returned pending last time.  Transition to completed.
+                snapshot.status = 'completed'
+                del(self.transition_to_completed[snapshot_id])
+            else:
+                # Transition to completed next time.
+                self.transition_to_completed[snapshot_id] = True
+        return snapshot
+
+    def create_snapshot(self, volume_id, name=None, description=None):
+        snapshot = Snapshot()
+        snapshot.id = _new_id()
+        snapshot.status = 'pending'
+        self.snapshots[snapshot.id] = snapshot
+        return snapshot
+
+    def delete_volume(self, volume_id):
+        del(self.volumes[volume_id])
+
+    def validate_guest_ami(self, ami_id):
+        pass
+
+    def validate_encryptor_ami(self, ami_id):
+        pass
+
+    def register_image(self,
+                       kernel_id,
+                       block_device_map,
+                       name=None,
+                       description=None):
+        image = Image()
+        image.id = _new_id()
+        image.block_device_mapping = block_device_map
+        image.state = 'available'
+        image.name = name
+        image.description = description
+        self.images[image.id] = image
+        return image.id
+
+    def wait_for_image(self, image_id):
+        pass
+
+    def get_image(self, image_id):
+        return self.images[image_id]
+
+    def delete_snapshot(self, snapshot_id):
+        del(self.snapshots[snapshot_id])
+
+
+class TestSnapshotProgress(unittest.TestCase):
 
     def test_snapshot_progress_text(self):
         # One snapshot.
@@ -25,6 +191,9 @@ class TestSoloCli(unittest.TestCase):
             '1: 25%, 2: 50%',
             brkt_cli._get_snapshot_progress_text([s1, s2])
         )
+
+
+class TestEncryptedImageName(unittest.TestCase):
 
     def test_encrypted_image_suffix(self):
         s1 = brkt_cli._get_encrypted_suffix()
@@ -50,3 +219,34 @@ class TestSoloCli(unittest.TestCase):
         encrypted_name = brkt_cli._get_encrypted_image_name(name)
         self.assertEqual(128, len(encrypted_name))
         self.assertTrue(encrypted_name.startswith('Boogie nights'))
+
+
+class TestSmoke(unittest.TestCase):
+
+    def test_smoke(self):
+        aws_svc = DummyAWSService()
+
+        # Encryptor image
+        bdm = BlockDeviceMapping()
+        for n in (1, 2, 3, 5):
+            device_name = '/dev/sda%d' % n
+            bdm[device_name] = BlockDeviceType()
+        id = aws_svc.register_image(
+            kernel_id=None, name='Encryptor image', block_device_map=bdm)
+        encryptor_image = aws_svc.get_image(id)
+
+        # Guest image
+        bdm = BlockDeviceMapping()
+        bdm['/dev/sda1'] = BlockDeviceType()
+        id = aws_svc.register_image(
+            kernel_id=None, name='Encryptor image', block_device_map=bdm)
+        guest_image = aws_svc.get_image(id)
+
+        # Run the smoke test.
+        brkt_cli.SLEEP_ENABLED = False
+        brkt_cli.run(
+            aws_svc=aws_svc,
+            enc_svc=DummyEncryptorService(),
+            image_id=guest_image.id,
+            encryptor_ami=encryptor_image.id
+        )
